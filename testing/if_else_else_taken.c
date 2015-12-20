c413d9050dff86196c1134186a1754e4cf6b39fd
Marek Milkovič <milkovic.marek@gmail.com>
2015-12-20 16:37:34 +0100

    [Testing] While & If-Else tests

---
+++ testing/if_else_else_taken.c
@@ -0,0 +1,14 @@
+int main(void)
+{
+	if (0)
+	{
+		print("If taken");
+	}
+	else
+	{
+		print("Else taken");
+	}
+
+	print("\n");
+}
+
---
+++ testing/if_else_if_taken.c
@@ -0,0 +1,13 @@
+int main(void)
+{
+	if (1)
+	{
+		print("If taken");
+	}
+	else
+	{
+		print("Else taken");
+	}
+
+	print("\n");
+}
